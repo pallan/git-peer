3832fd5a498dca353d54a782f3393323b23711be
Peer Allan <peer.allan@shopify.com>
2014-09-08 12:36:53 -0500

    Adds the Cleanup command

--- lib/commands/cleanup.rb
+++ lib/commands/cleanup.rb
@@ -6,8 +6,19 @@ module Commands
     def run!
       super
 
+      get('git branch -vv').split("\n").each do |raw|
+        parts = parse_line(raw)
+
+        sys("git branch -d #{parts[1]}") if parts && parts[3].include?(': gone')
+      end
 
       return 0
+    end
+
+    protected
+
+    def parse_line(line)
+      /\A(.*?)\s+([\w\d]{7})\s\[(.*?)\]\s(.*)\z/.match(line.strip)
     end
 
   end
